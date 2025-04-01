8dc95c2844e86915cf25c4cc10d96dd46bcc913e
Sanjeev N <worldsansgamming@gmail.com>
2025-04-01 20:40:13 +0530

    cha

--- React webapp/src/Navbar/Navbar.jsx
+++ React webapp/src/Navbar/Navbar.jsx
@@ -4,13 +4,15 @@ import styles from './Navbar.module.css'
 function Navbar() {
     return(
         <div className = {styles.navcard}>
-            <navbar>
+            <nav className = {styles.navcard}>
                 <h2>Leaf Node Solutions</h2>
-                <button>About Us</button>
-                <button>Contact Us</button>
-                <button>Log In</button>
-                <button>Sign Up</button>
-            </navbar>
+                <div className = {styles.navcard}>
+                    <button>About Us</button>
+                    <button>Contact Us</button>
+                    <button>Log In</button>
+                    <button>Sign Up</button>
+                </div>
+            </nav>
         </div>
     );
 }
