ea5a5712a72138a05ee90fbc0429e64207c7893c
Sanjeev N <worldsansgamming@gmail.com>
2025-04-05 16:38:36 +0530

    sorting

--- React webapp/src/CourseList.jsx
+++ React webapp/src/CourseList.jsx
@@ -8,19 +8,29 @@ function CourseList(){
         {
              name :"Portfolio Website",
              abt:"Showcase Yourself",
-             price:"$200",
+             price: 200,
              img: port, 
              login:true
         },
         {
              name:"Mini Project",
              abt:"Showcase Yourself",
-             price:"$500 - $1000",
+             price:1000,
              img:mini, 
              login:true
-        }
+        },
+        {
+            name:"Landing Page",
+            abt:"Single page website",
+            price:400,
+            img:mini, 
+            login:true
+       }
     ]
 
+    courses.sort((x,y) => y.price-x.price)
+
+    const ProList2 = courses.filter((PriceList)=> PriceList.price<200)
     const ProList = courses.map(
     (pro)=> <Products name={pro.name}
                       abt ={pro.abt}
